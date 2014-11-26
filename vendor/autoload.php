c75f649ecbb7b8bf96dc47dcff109744e4374ee6
faragelfadaly <eng.faragelfadaly@gmail.com>
2014-11-26 02:15:46 +0200

    add more files

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit40082223b9fbd6577fd317942de7d552::getLoader();
+return ComposerAutoloaderInit26bfe81b5a0c8640be5dba804131af67::getLoader();
